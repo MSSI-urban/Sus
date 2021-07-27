09c8bfb93d1f8457ef6e60cb4e1aafda9139eaf6
gracelongson <grace.longson@mail.mcgill.ca>
2021-07-27 09:58:04 -0400

    Updated DMTI script with Healthy Food Access metrics, DA values derived from DA buffers, and CT/borough values derived from a weighted average of the DA buffer values.

--- dev/modules/dmti.R
+++ dev/modules/dmti.R
@@ -1,122 +1,197 @@
 #### DMTI data setup ###########################################################
 
-# Get data ----------------------------------------------------------------
+library(tidyverse)
+library(sf)
+library(stats)
 
-# Original read of data, not kept for space reasons
-# dmti <- 
-#   read_sf("dev/data/dmti_2020/dmti_2016.shp") %>% 
-#   st_transform(4326) %>% 
-#   st_set_agr("constant") %>% 
-#   select(UID, NAME, ADDRESS, CITY, MUNICIPAL, POST_CODE, SIC_1, SIC_DIV)
-# qsave(dmti, "dev/data/dmti_2016.qs")
+# Get and clean data ------------------------------------------------
 
-# Read DMTI shapefiles and add 'year' column
-
-dmti_2006 <- st_read("dev/data/dmti_shp/dmti_2006.shp") %>% 
-  mutate(year = 2006, .before = geometry)
-dmti_2011 <- st_read("dev/data/dmti_shp/dmti_2011.shp") %>% 
-  mutate(year = 2011, .before = geometry) 
-dmti_2016 <- st_read("dev/data/dmti_shp/dmti_2016.shp") %>% 
-  mutate(year = 2016, .before = geometry) 
-dmti_2020 <- st_read("dev/data/dmti_shp/dmti_2020.shp") %>% 
-  mutate(year = 2020, .before = geometry)
-
-# transform df, select relevant variables
-
-clean_dmti <- function(d) {
-  d %>%
-    st_transform(32618) %>% 
-    st_set_agr("constant") %>% 
-    select(poi_id = POI_ID, name = NAME, address = ADDRESS, city = CITY, 
-           post_code = POST_CODE, sic_1 = SIC_1, sic_div = SIC_DIV, year)
+load_clean_dmti <- function(path) {
+  d <-
+    # load df
+    st_read(path) %>%
+    mutate(year = str_extract(path, "[[:digit:]]+"), .before = geometry) %>%
+    # transform df, select relevant variables
+    st_transform(32618) %>%
+    st_set_agr("constant") %>%
+    select(poi_id = POI_ID, name = NAME, address = ADDRESS, city = CITY,
+           post_code = POST_CODE, sic_1 = SIC_1, sic_div = SIC_DIV, year) %>%
+    mutate(sic_1 = as.integer(unlist(sic_1)),
+           year = as.integer(unlist(year)))
+  return (d)
 }
 
-dmti_2006 <- dmti_2006 %>% clean_dmti()
-dmti_2011 <- dmti_2011 %>% clean_dmti()
-dmti_2016 <- dmti_2016 %>% clean_dmti()
-dmti_2020 <- dmti_2020 %>% clean_dmti()
+dmti_2006 <- "C:/MSSI_GIS/DMTI/Github/Sus/dev/data/dmti_shp/dmti_2006.shp" %>% load_clean_dmti()
+dmti_2011 <- "C:/MSSI_GIS/DMTI/Github/Sus/dev/data/dmti_shp/dmti_2011.shp" %>% load_clean_dmti()
+dmti_2016 <- "C:/MSSI_GIS/DMTI/Github/Sus/dev/data/dmti_shp/dmti_2016.shp" %>% load_clean_dmti()
 
-# Row bind all DMTI data sets to create new df, remove old dfs
+# row bind all DMTI data sets to create new df, remove old dfs
 
-dmti <- rbind(dmti_2006, dmti_2011, dmti_2016, dmti_2020)
-rm(dmti_2006, dmti_2011, dmti_2016, dmti_2020)
-
-
-# Define filter vectors ---------------------------------------------------
-
-filter_vectors <- c("healthy_food_prop", "walkable_prop")
-
-healthy_food <- c("54110000", "54210000", "54310000", "54990000")
-unhealthy_food <- c("54410000", "54510000", "54610000", "58120000", "58130000", 
-                    "59120000", "59210000")
-walkable <- c("52310000", "52510000", "53110000", "53310000", "53990000",
-            "54110000", "54210000", "54310000", "54410000", "54510000",
-            "54610000", "54990000", "56110000", "56210000", "56320000",
-            "56410000", "56510000", "56610000", "56990000", "57140000",
-            "57190000", "57310000", "57340000", "57350000", "57360000",
-            "58120000", "58130000", "59120000", "59210000", "59410000",
-            "59420000", "59430000", "59440000", "59450000", "59460000",
-            "59470000", "59480000", "59490000", "59620000", "59920000",
-            "59930000", "59940000", "59950000")
-non_walkable <- c("52110000", "52610000", "52710000", "55110000", "55210000",
-                 "55310000", "55410000", "55510000", "55610000", "55710000",
-                 "55990000", "57120000", "57130000", "57220000", "59320000",
-                 "59610000", "59830000", "59840000", "59890000", "59990000",
-                 "59630000")
-healthy_city <- c("01810000", "07420000", "40110000", "41110000", "54110000", 
-                  "54310000", "59120000", "59950000", "72310000", "72410000", 
-                  "72990000", "78320000", "79220000", "79910000", "79970000", 
-                  "799990000", "80110000", "80210000", "80310000", "80410000", 
-                  "80420000", "80430000", "80490000", "80510000", "80520000", 
-                  "80590000", "80620000", "80630000", "80690000", "80710000", 
-                  "80720000", "80820000", "80920000", "80930000", "80990000", 
-                  "83220000", "83510000", "86410000", "92240000")
-unhealthy_city <- c("29110000", "29990000", "34330000", "55110000", "55210000",
-                    "55310000", "55410000", "55510000", "55610000", "55710000", 
-                    "55990000", "58130000", "59210000", "59830000", "59840000", 
-                    "59890000", "59930000", "75140000", "75210000", "75320000", 
-                    "75330000", "75340000", "75360000", "75370000", "75380000", 
-                    "75390000", "76990000")
-commercial_gentrification <- c("54110000", "54210000", "54310000", "54410000", 
-                               "54510000", "54610000", "54990000", "58120000", 
-                               "58130000", "59120000", "59210000")
+dmti <- rbind(dmti_2006, dmti_2011, dmti_2016)
+rm(dmti_2006, dmti_2011, dmti_2016)
 
 
-# Filter data -------------------------------------------------------------
 
-process_DMTI <- function(x) {
-  points <- 
-    dmti %>% 
-    st_join(x) %>% 
-    st_drop_geometry() %>% 
-    group_by(ID) %>% 
-    summarize(
-      healthy_food_prop = sum(SIC_1 %in% healthy_food) / 
-        (sum(SIC_1 %in% healthy_food) + sum(SIC_1 %in% unhealthy_food)),
-      walkable_prop = sum(SIC_1 %in% walkable) / 
-        (sum(SIC_1 %in% walkable) + sum(SIC_1 %in% non_walkable))) 
+
+# Create 'type' variable ---------------------------------------
+
+# define SIC code strings
+
+healthy_food_sic <- c("54110000", "54210000", "54310000", "54990000")
+unhealthy_food_sic <- c("54410000", "54510000", "54610000", "58120000", 
+                        "58130000", "59210000")
+
+# conditionally add values to 'type' column
+
+dmti_test <- 
+  dmti %>% 
+  mutate(type = case_when(sic_1 %in% healthy_food_sic ~ "foodhealthy",
+                          (sic_1 %in% unhealthy_food_sic) | 
+                            (grepl("jean coutu", name, ignore.case = TRUE) |  
+                               grepl("uniprix", name, ignore.case = TRUE) |  
+                               grepl("pharmaprix", name, ignore.case = TRUE)) ~ "foodunhealthy"),
+         .before = geometry) %>% 
+  # remove rows that are not relevant to any modules to improve processing time
+  drop_na(type)
+
+
+
+
+# Process data ---------------------------------------------------------------
+
+#process_dmti <- function(x) {
+DA_test <- 
+  DA %>% 
+  st_set_geometry("buffer") %>% 
+  st_transform(32618) %>% 
+  select(ID) %>% 
+  st_join(., dmti_test) %>%
+  st_drop_geometry() %>% 
+  count(ID, year, type) %>%
+  filter(!is.na(type)) %>%
+  tidyr::pivot_wider(id_cols = "ID",
+                     names_from = c("type", "year"),
+                     names_sep = "_",
+                     values_from = n) %>% 
+  left_join(DA, ., by = "ID") %>%
   
+  relocate(starts_with("food"), .before = geometry) %>%
+  mutate(across(starts_with("food"), 
+                .fns = list(
+                  prop = ~{.x /  }
+                )))
+  st_as_sf() %>%
+  st_set_agr("constant")
+#}
+
+#
+left_join(join_results, by = "ID") |> 
+  relocate(starts_with("crash"), .before = geometry) |> 
+  mutate(across(starts_with("crash"), 
+                .fns = list(
+                  prop_area = ~{.x / units::drop_units(st_area(geometry))},
+                  prop_pop = ~{.x / population}),
+                .names = "{.col}_{.fn}"), .before = geometry) |> 
+  mutate(across(starts_with("crash"), ntile, n = 3, .names = "{.col}_q3"), 
+         .before = geometry) %>% 
+  st_set_agr("constant")
+#
+
+
+DA_data <-
+  DA_test %>% 
+  select(ID, CTUID, CSDUID, population, starts_with("food")) %>% 
+  mutate(
+    foodhealthy_prop_2006 = foodhealthy_2006 / (foodhealthy_2006 + foodunhealthy_2006),
+    foodhealthy_prop_2011 = foodhealthy_2011 / (foodhealthy_2011 + foodunhealthy_2011),
+    foodhealthy_prop_2016 = foodhealthy_2016 / (foodhealthy_2016 + foodunhealthy_2016),
+    food_prop_popthous_2006 = (foodhealthy_2006 + foodunhealthy_2006) / (population / 1000),
+    food_prop_popthous_2011 = (foodhealthy_2011 + foodunhealthy_2011) / (population / 1000),
+    food_prop_popthous_2016 = (foodhealthy_2016 + foodunhealthy_2016) / (population / 1000),
+    foodhealthy_prop_popthous_2006 = foodhealthy_2006 / (population / 1000),
+    foodhealthy_prop_popthous_2011 = foodhealthy_2011 / (population / 1000),
+    foodhealthy_prop_popthous_2016 = foodhealthy_2016 / (population / 1000),
+    .before = geometry) %>% 
+  st_set_agr("constant")
+
+#CT_from_DAbuff <-
+DA_data %>%
+  st_drop_geometry() %>%
+  select(CTUID, population, starts_with("food")) %>%
+  group_by(CTUID) %>%
+  summarize(across(starts_with("food"),
+                   .fns = list(
+                     weighted_mean = ~weighted.mean(.x, population, na.rm=TRUE)),
+                   .names = "{.col}_{.fn}")) %>% 
+  left_join(CT, ., by = c("ID" = "CTUID")) %>%
+  relocate(contains(c("healthyfood", "food_prop")), .before = geometry) %>%
+  mutate(across(contains(c("healthyfood","food_prop")), ntile, n = 3, .names = "{.col}_q3"),
+         .before = geometry) %>%
+  st_set_agr("constant")
+
+borough_from_DAbuff <- 
+  # identical to DT_from_DAbuff except replace "CTUID" with "CSDUID"
+  
+  
+  # METRIC CREATION ---------------------------------------------------------
+
+process_dmti <- function(x) {
   x %>% 
+    st_transform(32618) %>%
+    st_join(., dmti) %>% 
+    st_drop_geometry() %>% 
+    group_by(ID, year) %>% 
+    filter(!is.na(type)) %>% 
+    print() %>% 
+    summarize(
+      healthyfood_prop_2006 = healthyfood_2006 / (healthyfood_2006 + unhealthyfood_2006),
+      healthyfood_prop_2011 = healthyfood_2011 / (healthyfood_2011 + unhealthyfood_2011),
+      healthyfood_prop_2016 = healthyfood_2016 / (healthyfood_2016 + unhealthyfood_2016)
+      ,
+      #   healthy_food_prop_2020 = healthyfood_2020 / (healthyfood_2020 + unhealthyfood_2020),
+      food_prop_popthous_2006 = (healthyfood_2006 + unhealthyfood_2006) / (x$population / 1000),
+      food_prop_popthous_2011 = (healthyfood_2011 + unhealthyfood_2011) / (x$population / 1000),
+      food_prop_popthous_2016 = (healthyfood_2016 + unhealthyfood_2016) / (x$population / 1000)
+      ,
+      #  food_prop_popthous_2020 = (healthyfood_2020 + unhealthyfood_2020) / (x$population / 1000),
+      healthyfood_prop_popthous_2006 = healthyfood_2006 / (x$population / 1000),
+      healthyfood_prop_popthous_2011 = healthyfood_2011 / (x$population / 1000),
+      healthyfood_prop_popthous_2016 = healthyfood_2016 / (x$population / 1000),
+      # food_prop_popthous_2020 = (healthyfood_2020 + unhealthyfood_2020) / (x$population / 1000)
+    ) %>%
     left_join(points, by = "ID") %>% 
-    relocate(all_of(filter_vectors), .before = geometry) %>% 
-    mutate(across(filter_vectors, ntile, n = 3, .names = "{.col}_q3"), 
-           .before = geometry) %>% 
+    relocate(contains("food_prop"), .before = geometry) %>% 
+    mutate(across("food_prop", ntile, n = 3, .names = "{.col}_q3"), 
+           .before = geometry) %>% # new tertile syntax
     st_set_agr("constant")
 }
 
-points <- map(list(borough, CT, DA, grid), process_DMTI)
-
-
 # Add to existing geographies ---------------------------------------------
 
-borough <- points[[1]]
-CT <- points[[2]]
-DA <- points[[3]]
-grid <- points[[4]]
+dmti_geom_list <- map(list(borough, CT, DA, grid), dmti_geoadd_fun)
+
+borough <- dmti_geom_list[[1]]
+CT <- dmti_geom_list[[2]]
+DA <- dmti_geom_list[[3]]
+grid <- dmti_geom_list[[4]]
+
+dmti_metrics_list <- map(list(borough, CT, DA, grid), process_dmti)
+
+borough <- dmti_geom_list[[1]]
+CT <- dmti_geom_list[[2]]
+DA <- dmti_geom_list[[3]]
+grid <- dmti_geom_list[[4]]
 
 rm(dmti, points, filter_vectors, healthy_food, non_walkable, unhealthy_food,
    walkable, healthy_city, unhealthy_city, commercial_gentrification,
    process_DMTI)
+
+
+# RETURN SCALE GEOMETRIES TO 'GEOMETRY' COLUMN ----------------------------
+
+DA %>% 
+  st_set_geometry("geometry")
+
 
 
 # Add variable explanations -----------------------------------------------
